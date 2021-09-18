9a4664e56d53c15067a7a4e888cbce5a202a25fe
Imran Rahman <mrimranoor@gmail.com>
2021-09-18 14:49:03 +0100

    connection to mongoDB

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = requrie("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  });
+  console.log("Conected to MongoDB");
+};
+
+module.exports = connectDB;
