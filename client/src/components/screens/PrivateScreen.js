069ca8e5d6b32a85aa9717aedc7f4dce1290015d
Imran Rahman <mrimranoor@gmail.com>
2021-09-22 01:29:34 +0100

    fixed bug with resetpassword

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -1,46 +1,35 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import "./PrivateScreen.css";
+import "./PrivateScreen.css";
 
-const PrivateScreen = ({ history }) => {
+const PrivateScreen = () => {
   const [error, setError] = useState("");
   const [privateData, setPrivateData] = useState("");
 
   useEffect(() => {
-    if (!localStorage.getItem("authToken")) {
-      history.push("/login");
-    }
-    const fetchPrivateData = async () => {
+    const fetchPrivateDate = async () => {
       const config = {
-        header: {
+        headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${localStorage.getItem("authToken")}`,
         },
       };
 
       try {
-        const { data } = await axios.get("/apli/private", config);
+        const { data } = await axios.get("/api/private", config);
         setPrivateData(data.data);
       } catch (error) {
         localStorage.removeItem("authToken");
-        setError("You are unauthorised to access this, please login...");
+        setError("You are not authorized please login");
       }
     };
-    fetchPrivateData();
-  }, [history]);
 
-  const logoutHandler = () => {
-    localStorage.removeItem("authToken");
-    history.push("/login");
-  };
-
+    fetchPrivateDate();
+  }, []);
   return error ? (
-    <span className="error-message"></span>
+    <span className="error-message">{error}</span>
   ) : (
-    <>
-      <div style={{ background: "green", colour: "white" }}>{privateData}</div>
-      <button onClick={logoutHandler}>Logout</button>
-    </>
+    <div>{privateData}</div>
   );
 };
 
